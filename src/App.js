451e6d4beddf41f109bd54b26be1024f71107577
Trideep555 <71317868+Trideep555@users.noreply.github.com>
2023-05-13 15:35:34 +0530

    Adding Dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/login';
+import Sidebar from './components/Sidebar';
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Sidebar />
           </div>
   );
 }
